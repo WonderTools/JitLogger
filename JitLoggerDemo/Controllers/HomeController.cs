6a1c8044d37f632e9b8e2f530d5dd1e76ecb6ffe
Nachiappan <nachiappan.nk.ext@sivantos.com>
2018-09-09 11:18:31 +0530

    Added a sample log

--- JitLoggerDemo/Controllers/HomeController.cs
+++ JitLoggerDemo/Controllers/HomeController.cs
@@ -4,14 +4,23 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Nachiappan.JitLoggerDemo.Models;
 
 namespace Nachiappan.JitLoggerDemo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
+            _logger.LogError("Not an error. But logging as an error for ");
             return View();
         }
 

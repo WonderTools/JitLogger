de5bae7ab086648896983ae61d9c8013979157d0
Nachiappan <nachiappan.nk.ext@sivantos.com>
2018-09-18 22:51:34 +0530

    Updated the usage project

--- JitLoggerSampleApplications/BasicUsageOfJitLogger/BasicUsageOfJitLogger/Startup.cs
+++ JitLoggerSampleApplications/BasicUsageOfJitLogger/BasicUsageOfJitLogger/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Nachiappan.JitLogger;
+using WonderTools.JitLogger;
 
 namespace BasicUsageOfJitLogger
 {
